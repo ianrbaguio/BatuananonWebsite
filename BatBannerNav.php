d186d1414e22e9fb321a155382be67871635b58a
rcmbaguio <48397376+rcmbaguio@users.noreply.github.com>
2019-03-16 18:45:04 -0600

    Add files via upload

---
+++ BatBannerNav.php
@@ -0,0 +1,18 @@
+<div id ="banner">
+			
+				<ul id="mainlist">
+					<li id="bannerimg"><img src ="batuananon.png" alt="batuananon"></li>
+					<li class="listitems"><a href="BatFrontPage.php">Home</a></li>
+					<li class="listitems">Events</li>
+					<li class="listitems">Contacts</li>
+				</ul>
+				
+				<ul id="topright">
+					<li> Sign-in </li>
+					<li> <a href="BatRegistrationPage.php">Register </a> </li>
+				</ul>
+		
+			
+		
+			</div>
+		
